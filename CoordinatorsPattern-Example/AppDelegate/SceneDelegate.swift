00d8a8808c2928e9e9181d95e0f9aeeb9f590bdf
Petre Vane <PetreVane@users.noreply.github.com>
2020-02-10 19:54:40 +0100

    Coordinator takes control over the app flow from SceneDelegate; window is manually created.

--- CoordinatorsPattern-Example/AppDelegate/SceneDelegate.swift
+++ CoordinatorsPattern-Example/AppDelegate/SceneDelegate.swift
@@ -15,10 +15,25 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+ 
+        // creates the main navigation controller to be used for our app
+        let navigationController = UINavigationController()
+        
+        // passes that into our coordinator so that it can display view controllers
+        coordinator = MainCoordinator(navigationController: navigationController)
+        
+        // calls the start method, and by doing so, asking the coordinator to take over control
+        coordinator?.start()
+        
+        // give the scene a name
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // creates a basic window
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
